4dcc9aaa5d2bbfd58743bfff3a1263d12f14f483
R.Kalai Maran <kalaimaranr18@gmail.com>
2025-08-24 22:41:37 +0530

    Add files via upload

---
+++ DAY1/Demo 3.sql
@@ -0,0 +1,16 @@
+show databases;
+use pc_project;
+create table country_details(sno int, name varchar(100),capital varchar(100),
+people_count int);
+select*from country_details;
+insert into country_details values(3,'America','New york',4900000),
+(4,'Sri lanka','kozhumbu',3800000);
+select*from country_details;
+
+select name from country_details;
+select*from country_details;
+
+
+
+
+
